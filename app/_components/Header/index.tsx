8b4ca7d357605c9f734f97cf28168df05f6c32c6
nextgamelimited <nextgamelimited2025@gmail.com>
2026-03-02 06:28:23 +0900

    Refactor Header component to include logo and menu

--- app/_components/Header/index.tsx
+++ app/_components/Header/index.tsx
@@ -1,10 +1,27 @@
-import Menu from '@/app/_components/Menu';
-import styles from './index.module.css';
+import Menu from '@/app/_components/Menu'
+import styles from './index.module.css'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <Menu />
+      <div className={styles.inner}>
+
+        {/* ロゴ */}
+        <Link href="/" className={styles.logo}>
+          <Image
+            src="/images/logo.png"
+            alt="NEXTGAME"
+            width={120}
+            height={40}
+          />
+        </Link>
+
+        {/* メニュー */}
+        <Menu />
+
+      </div>
     </header>
-  );
+  )
 }
