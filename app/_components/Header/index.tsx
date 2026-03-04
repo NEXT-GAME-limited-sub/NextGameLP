dc85000cc9832a647199dc0d86807ee30822bc5a
nextgamelimited <nextgamelimited2025@gmail.com>
2026-03-04 19:59:19 +0900

    ヘッダーにロゴを追加

--- app/_components/Header/index.tsx
+++ app/_components/Header/index.tsx
@@ -1,23 +1,22 @@
-　import Menu from '@/app/_components/Menu';
+import Menu from '@/app/_components/Menu';
 import styles from './index.module.css';
-import Image from "next/image";
-import Link from "next/link";
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Header() {
   return (
     <header className={styles.header}>
-
       <Link href="/">
         <Image
           src="/logo.png"
           alt="NEXTGAME"
           width={140}
           height={40}
+          priority
         />
       </Link>
 
       <Menu />
-
     </header>
   );
 }
